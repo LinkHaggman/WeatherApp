4666d1959e9184a0add1a9bfec76df4dd15b8578
Link <66062268+Link0v0@users.noreply.github.com>
2021-07-28 10:45:18 +0200

    Add files via upload

---
+++ dist/precache-manifest.1c2c2696fac3a44d3d6ea91c9c50ac00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f0cb7fd4250e0ce9cca",
+    "url": "/css/app.a4116f4c.css"
+  },
+  {
+    "revision": "25f6a5425d0a23306f106facdafd2e4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f0cb7fd4250e0ce9cca",
+    "url": "/js/app.76cedd04.js"
+  },
+  {
+    "revision": "4d13249852671be219f5",
+    "url": "/js/chunk-vendors.f47b700a.js"
+  },
+  {
+    "revision": "a1038c3003eae0312503957290342b26",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
